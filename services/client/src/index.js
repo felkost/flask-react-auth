b1dd6300c3200d95256b4afc4464f25282b983fc
felkost <felkost@ukr.net>
2020-09-27 19:28:55 +0300

    Add React Form connected with API

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-        users: []
+        users: [],
+        username: '',
+        email: ''
     };
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   };
 
   componentDidMount() {
@@ -22,6 +27,30 @@ class App extends Component {
     .catch((err) => { console.log(err); });
   }
 
+//    addUser(event) {
+//      event.preventDefault();
+//      console.log('sanity check!');
+//    };
+    addUser = (event) => {
+      event.preventDefault();
+      const data = {
+        username: this.state.username,
+        email: this.state.email
+      };
+      axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => {
+            this.getUsers();
+            this.setState({ username: '', email: '' });
+       })
+      .catch((err) => { console.log(err); });
+        };
+
+    handleChange(event) {
+      const obj = {};
+      obj[event.target.name] = event.target.value;
+      this.setState(obj);
+    };
+
   render() {
     return (
       <section className="section">
@@ -31,6 +60,13 @@ class App extends Component {
               <br/>
               <h1 className="title is-1">Users</h1>
               <hr/><br/>
+              <AddUser
+                  username={this.state.username}
+                  email={this.state.email}
+                  addUser={this.addUser}
+                  handleChange={this.handleChange}
+              />
+              <br/><br/>
               <UsersList users={this.state.users}/>
             </div>
           </div>
